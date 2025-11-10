ae289c8517fa0af9031bd8c46fa3795133b07bb3
matheus <matheuscf6@gmail.com>
2025-11-10 11:18:31 -0300

    corre;ao de bug

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
 
     int opcao;
-    Fila* fila_cliente = criar_fila();
+    Fila *fila_cliente = criar_fila();
 
-    do{
+    do
+    {
         printf("Sistema de Gerenciamento de Fila de Pacientes\n");
         printf("--------------------------------------------\n");
         printf("Selecione uma opção:\n");
@@ -18,8 +20,9 @@ int main() {
         scanf("%d", &opcao);
         limpar_buffer();
 
-        switch (opcao){
-            case 1:
+        switch (opcao)
+        {
+        case 1:
             // Código para adicionar paciente à fila
 
             paciente pac;
@@ -27,8 +30,8 @@ int main() {
             printf("Adicionar paciente.\n");
             printf("--------------------------------------------\n");
             printf("Digite o nome do paciente: ");
-            scanf("%49[^\n]", pac.nome); 
-            limpar_buffer(); 
+            scanf("%49[^\n]", pac.nome);
+            limpar_buffer();
 
             enfileirar(fila_cliente, pac);
             printf("Paciente adicionado à fila com sucesso!\n");
@@ -37,18 +40,20 @@ int main() {
             printf("Aperte ENTER para retornar ao menu.\n");
             getchar();
 
-                break;
+            break;
 
-            case 2:
+        case 2:
             // Código para atender próximo paciente
 
             printf("Atender próximo paciente.\n");
             printf("--------------------------------------------\n");
 
-            if (lista_vazia(fila_cliente)) {
+            if (lista_vazia(fila_cliente))
+            {
                 printf("A fila está vazia. Nenhum paciente para atender.\n\n");
-
-            } else {
+            }
+            else
+            {
                 paciente atendido = desenfileirar(fila_cliente);
                 printf("Paciente atendido:\n");
                 print_paciente(atendido);
@@ -57,48 +62,48 @@ int main() {
                 paciente proximo = fila_cliente->inicio->dados;
                 print_paciente(proximo);
                 printf("\n");
-                
             }
 
             printf("Aperte ENTER para retornar ao menu.\n");
             getchar();
 
-                break;
+            break;
 
-            case 3:
+        case 3:
             // Código para exibir fila de pacientes
 
             printf("Exibir fila de pacientes.\n");
             printf("--------------------------------------------\n");
 
-            if (lista_vazia(fila_cliente)) {
+            if (lista_vazia(fila_cliente))
+            {
                 printf("A fila está vazia.\n");
-
-            } else {
+            }
+            else
+            {
                 printf("Pacientes na fila de espera:\n");
                 imprimir_fila(fila_cliente);
-
             }
 
             printf("Aperte ENTER para retornar ao menu.\n");
             getchar();
-            
-                break;
 
-            case 4:
+            break;
+
+        case 4:
             // Código para sair
 
             printf("Saindo do sistema. Até logo!\n");
             exit(0);
 
-                break; 
+            break;
 
-            default:
-                break;
+        default:
+            break;
         }
 
     } while (opcao != 4);
-        printf("Opção inválida. Tente novamente.\n");
+    printf("Opção inválida. Tente novamente.\n");
 
     return 0;
 }
---
+++ modules/fila.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{           // Define a struct para armazenar os dados do paciente.
+    int id; // ID do paciente. -->IDENTIFICAÇÃO DO PACIENTE<--
+    char nome[50];
+} paciente;
+
+typedef struct elemento_fila
+{ // Define a struct para os elementos da fila.
+    paciente dados;
+    struct elemento_fila *prox;
+} Elemento;
+
+typedef struct fila
+{ // Define a struct para a fila em si. -->CONTROLE DA FILA<--
+    Elemento *inicio;
+    Elemento *fim;
+    int tamanho;
+} Fila;
+
+// protótipos das funções
+Fila *criar_fila();
+paciente adicionar_paciente(const char *nome);
+int lista_vazia(Fila *fila);
+void liberar_fila(Fila *fila);
+void enfileirar(Fila *fila, paciente dados);
+void imprimir_fila(Fila *fila);
+int tamanho_fila(Fila *fila);
+paciente desenfileirar(Fila *fila);
+void print_paciente(paciente paciente);
+void limpar_buffer();
